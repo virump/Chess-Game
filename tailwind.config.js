241f88e8480754f4549e3d4fff0e8d0bed33697f
virump <pathakvirru@gmail.com>
2024-12-22 19:01:22 +0530

    Fix line ending warnings

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./views/index.ejs', './public/js/chessgame.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
